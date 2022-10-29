4dd2515cc7a1dda6361c9d9c820a9ccdd202b438
Adil Ali <aadilali444@.com>
2022-10-29 12:00:05 +0530

    ui

--- script/script.js
+++ script/script.js
@@ -3,23 +3,16 @@ const search_container = document.querySelector('.search-container');
 const search_icon = document.querySelector('.search');
 const nav = document.querySelector('.navigation')
 search_icon.style.cursor = 'pointer';
-
-cross.addEventListener('mouseover', () => {
-    cross.style.transform = 'rotate(18deg)';
-
-
-    console.log('Adil ')
-})
-
-window.onscroll = function() { scrolli() };
+const NavigationS = nav.offsetTop;
+// const su = search_container.className = "search-up";
 
 function scrolli() {
 
-    if (scrollY > 350) {
-        nav.className = 'navigationS';
+    if (window.scrollY >= NavigationS) {
+        nav.classList.add("NavigationS")
         console.log('work')
     } else {
-        nav.className = 'navigation';
+        nav.classList.remove('NavigationS');
     }
 
 
@@ -29,15 +22,14 @@ function scrolli() {
 
 search_icon.addEventListener('click', () => {
 
-
     search_container.classList = 'search-down';
 
 
     console.log('hello');
 });
 cross.addEventListener('click', () => {
-    search_container.className = "search-up";
 
+    search_container.className = "search-up";
     // search_container.className = 'search-container';
 
 
